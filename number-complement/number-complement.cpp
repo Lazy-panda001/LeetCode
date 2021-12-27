e48e4b2f4bd71c7f26817eadc696bdd8ab372a69
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2021-12-27 17:51:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (6.38%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findComplement(int num) 
+    {
+        vector<int> vec;
+        while(num !=0)
+        {
+            vec.push_back(num % 2);
+            num = num / 2;
+        }
+        
+        reverse(vec.begin(), vec.end());
+        
+        for(int i=0; i<vec.size(); i++)
+        {
+            if(vec[i] == 0)
+            {
+                vec[i] = 1;
+            }
+            else
+                vec[i] = 0;
+        }
+        
+        
+        long long  two=1;
+        long long sol = 0;
+        
+        for(int i=vec.size()-1; i>=0; i--)
+        {
+            sol = sol + vec[i] *two;
+            two = two*2;
+            
+        }
+        return sol;
+        
+    }
+};
