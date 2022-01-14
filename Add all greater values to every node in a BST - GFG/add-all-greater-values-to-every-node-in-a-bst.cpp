42582ec7a7290d125da3b2e04b20213cc5f9f71b
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-14 19:57:11 +0530

    Added solution - LeetHub

---
+++ Add all greater values to every node in a BST - GFG/add-all-greater-values-to-every-node-in-a-bst.cpp
@@ -0,0 +1,195 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+Node* modify(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+       //getline(cin, s);
+        // int k = stoi(s);
+        // getline(cin, s);
+
+        root = modify(root);
+        vector<int> v;
+        inorder(root, v);
+        for(auto i:v)
+            cout << i << " ";
+        cout << endl;
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}// } Driver Code Ends
+
+
+/*
+Node structure is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+Node* makeBST(vector<int>& nums, int start , int end)
+{
+    if(start > end)
+    {
+        return nullptr;
+    }
+    
+    int mid = start + (end - start)/2;
+    
+    Node* root = new Node(nums[mid]);
+    
+    root->left = makeBST(nums , start, mid-1);
+    
+    root->right = makeBST(nums , mid+1 , end);
+    
+    return root;
+}
+
+
+void inOrder(Node* root , vector<int> &inorder)
+{
+    if(root == nullptr)
+    {
+        return ;
+    }
+    
+    inOrder(root->left , inorder);
+    
+    inorder.push_back(root->data);
+    
+    inOrder(root->right , inorder);
+    
+    return;
+}
+
+// modify the BST and return its root
+Node* modify(Node *root)
+{
+    // Your code here
+    vector<int> inorder;
+    
+    inOrder(root , inorder);
+    
+    int n = inorder.size();
+    
+    vector<int> suffix_sum(n);
+    
+    suffix_sum[n-1] = inorder[n-1];
+    
+    for(int i=n-2; i>=0; i--)
+    {
+        suffix_sum[i] = suffix_sum[i+1] + inorder[i];
+    }
+    
+    return makeBST(suffix_sum , 0, n-1);
+    
+    
+    
+}
