551d804035f6c01317f673a956240dc55ef34807
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-17 10:54:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.50%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) 
+    {
+        int n = str.size();
+        string temp = "";
+        vector<string> res;
+        int i=0;
+        while(i<n)
+        {
+            if(str[i] == ' ')
+            {
+                res.push_back(temp);
+                temp ="";
+            }
+            else
+            {
+                temp+=str[i];
+            }
+        i++;
+        }
+        
+        res.push_back(temp);
+        if(res.size() != pattern.length())
+        {
+            return false;
+        }
+        
+        unordered_map<char , string>ump;
+        set<string> st;
+        
+        for(int i=0; i<res.size(); i++)
+        {
+            if(ump.count(pattern[i]) == 0 and st.count(res[i]) == 0) // one - one mapping
+            {
+                ump[pattern[i]] = res[i];
+                st.insert(res[i]);
+            }
+            
+            else if(ump.count(pattern[i]) == 0 and st.count(res[i]) != 0)
+            {
+                return false;
+            }
+            else if(ump[pattern[i]] != res[i])
+            {
+                return false;
+            }
+            
+        }
+        return true;
+        
+    }
+};
