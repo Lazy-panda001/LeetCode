3d8374ceb9d38fbff01015647b6eabd80042ed61
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-19 22:10:04 +0530

    Time: 212 ms (26.04%), Space: 42.6 MB (37.93%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int find_length(ListNode* head)
+    {
+        int len =0;
+        while(head != nullptr)
+        {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+    
+    vector<int> nextLargerNodes(ListNode* head) 
+    {
+        
+        ListNode* temp = head;
+        int n = find_length(head);
+        vector<int> nums(n);
+        int k=0;
+        while(temp)
+        {
+            nums[k] = temp->val;
+            k++;
+            temp = temp->next;
+        }
+        
+        /*for(auto it:nums)
+        {
+            cout<<it<<" ";
+        }*/
+            
+        stack<int> stk;
+        vector<int>res;
+        
+        for(int i = n-1; i>=0; i--)
+        {
+            if(stk.size() == 0)
+            {
+                res.push_back(0);
+            }
+            else if(stk.size()>0 and stk.top()>nums[i])
+            {
+                int ans = stk.top();
+                res.push_back(ans);
+            }
+            else if(stk.size()>0 and stk.top()<=nums[i])
+            {
+                while(stk.size()>0 and stk.top()<=nums[i])
+                {
+                    stk.pop();
+                }
+                
+                // may be stack empty
+                if(stk.size() == 0)
+                {
+                    res.push_back(0);
+                }
+                
+                else if(stk.top()>nums[i])
+                {
+                    int ans = stk.top();
+                    res.push_back(ans);
+                }
+            }
+            
+            
+            stk.push(nums[i]);
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+        
+    }
+};
