0d975d39085b09e848fffd694c025bf071a1b6e7
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-20 18:56:09 +0530

    Time: 60 ms (44.07%), Space: 18.9 MB (16.35%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    bool isPossible(vector<int>& nums, int hour , int k)
+    {
+        long long total_time =0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            int div = nums[i] / k;
+            total_time +=div;
+            if(nums[i] % k != 0)
+            {
+                total_time++;
+            }
+          
+        }
+        
+        if(total_time <= hour)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int hour) 
+    {
+        int max_value = *max_element(piles.begin(),piles.end());
+       // cout<<max_value<<" ";
+        
+        int low=1;
+        int high = max_value;
+       // int k = 0;
+        
+        int ans=0;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+           // k = mid;
+            
+            if(isPossible(piles, hour , mid) == true)
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
