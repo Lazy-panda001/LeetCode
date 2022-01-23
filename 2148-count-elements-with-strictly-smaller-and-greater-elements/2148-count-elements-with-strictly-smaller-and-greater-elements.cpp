32053b8a44191eab47a01554385bd02624f494d2
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-23 11:21:49 +0530

    Time: 10 ms (22.22%), Space: 9.2 MB (11.11%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,31 +1,12 @@
-class Solution {
+class Solution 
+{
 public:
-    
-    bool all_same(vector<int>& nums)
-    {
-        int first = nums[0];
-        
-        for(int i=1; i<nums.size(); i++)
-        {
-            if(nums[i] != first)
-            {
-                return 0;
-            }
-        }
-        return 1;
-    }
-    
     
     int countElements(vector<int>& nums) 
     {
         int n = nums.size();
         
-        if(all_same(nums) == true)
-        {
-            return 0;
-        }
-        else
-        {
+        
         int max_val = *max_element(nums.begin(), nums.end());
         
         int min_val = *min_element(nums.begin(), nums.end());
@@ -33,9 +14,12 @@ public:
         int count_max = count(nums.begin(), nums.end(),max_val);
         
         int count_min = count(nums.begin(), nums.end(),min_val);
+        
+        if(max_val == min_val)
+        {
+            return 0;
+        }
 
         return (n - count_max - count_min);
-        }
     }
-    
 };
