62f2b90153161f1d3dc0c9e4b1ebfb4cdd00f4b5
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-24 10:52:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (67.65%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -2,62 +2,26 @@ class Solution {
 public:
     bool detectCapitalUse(string word) 
     {
+        int count=0;
         int n = word.size();
-        if(n == 1)
+        for(int i=1; i<n; i++)
+        {
+            if(word[i]>=97)
+            {
+                count++;
+            }
+        }
+        
+        if(count == n-1) // all small will count
         {
             return true;
         }
-        
-        // Case 1 : 1st letter is capital and all rest is small
-        if((word[0] >='A' and word[0]<='Z') and(word[1]>='a' and word[1]<='z'))
+        else if(count == 0 and word[0] >=65 and word[0] <= 90)
         {
-            for(int i=2; i<n; i++)
-            {
-                if(word[i]>='a' and word[i]<='z')
-                {
-                    continue;
-                }
-                else
-                    return false;
-            }
+            return true;
         }
-        
-        // Case 2 : 1st letter is small and rest all are also small
-       else if((word[0] >='a' and word[0]<='z') and(word[1]>='a' and word[1]<='z'))
-        {
-            for(int i=2; i<n; i++)
-            {
-                if(word[i]>='a' and word[i]<='z')
-                {
-                    continue;
-                }
-                else
-                    return false;
-            }
-        }
-        
-        // Case 3: 1st letter is caplital and rest all are also capital
-       else if((word[0] >='A' and word[0]<='Z') and(word[1]>='A' and word[1]<='Z'))
-        {
-            for(int i=2; i<n; i++)
-            {
-                if(word[i]>='A' and word[i]<='Z')
-                {
-                    continue;
-                }
-                else
-                    return false;
-            }
-        }
-        
-        else
-            return false;
-        return true;
-        
-        
+        return false;
+            
         
     }
-    
-    
-    
 };
