768a4abee718f2b1287a9f4fc1530ad92f6a979d
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-28 11:09:46 +0530

    Added solution - LeetHub

---
+++ Last index of One - GFG/last-index-of-one.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    int lastIndex(string str) 
+    {
+        int n = str.size();
+        
+        for(int i=n-1; i>=0;i--)
+        {
+            if(str[i] == '1')
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.lastIndex(s) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
