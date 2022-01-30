5fa6f9188128218c42e6cd35f39a78445b4c0114
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-30 19:22:11 +0530

    Time: 30 ms (80.73%), Space: 19.1 MB (41.05%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
@@ -2,61 +2,28 @@ class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) 
     {
-        vector<string> str1;
-        unordered_map<string , int> ump1;
+         unordered_map<string , int> ump1 , ump2;
         
         for(int i=0; i<words1.size(); i++)
         {
             ump1[words1[i]]++;
         }
         
-        for(auto it : ump1)
-        {
-            if(it.second == 1)
-            {
-                str1.push_back(it.first);
-            }
-            else
-            {
-                
-            }
-        }
-        
-        vector<string> str2;
-        unordered_map<string , int> ump2;
         for(int i=0; i<words2.size(); i++)
         {
             ump2[words2[i]]++;
         }
         
-         for(auto it : ump2)
-        {
-            if(it.second == 1)
-            {
-                str2.push_back(it.first);
-            }
-            else
-            {
-                
-            }
-        }
-        
-        
         int count=0;
         
-       for(int i=0; i<str1.size(); i++)
-       {
-           for(int j=0; j<str2.size(); j++)
-           {
-               if(str1[i] == str2[j])
-               {
-                   count++;
-               }
-           }
-       }
-            
-        
-        return count;;
+        for(auto it : ump1)
+        {
+            if(it.second == 1 and ump2[it.first] == 1)
+            {
+                count++;
+            }
+        }
+        return count;
         
     }
 };
