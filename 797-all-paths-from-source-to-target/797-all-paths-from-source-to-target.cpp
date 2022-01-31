3a83ff4b1e661e6f22bcaa2c4bf986bb2d9f26c9
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-31 20:46:45 +0530

    Time: 338 ms (5.26%), Space: 48 MB (5.14%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     vector<vector<int>> all_path;
     
-    void dfs(vector<int> adj_list[] , int src ,int des ,  vector<int>&each_path)
+    void dfs(vector<vector<int>> adj_list , int src ,int des ,  vector<int>&each_path)
     {
         
         if(src == des)
@@ -21,22 +21,22 @@ public:
     }
     
     
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj_list) 
     {
         vector<int> each_path;
         
-        int n = graph.size();
+        int n = adj_list.size();
         
-        vector<int> adj_list[n];
+      //  vector<int> adj_list[n];
         
-        for(int i=0; i<n; i++)  // it will make the adjacency list of the graph
+       /* for(int i=0; i<n; i++)  // it will make the adjacency list of the graph
         {
             for(int j=0;j<graph[i].size();j++)
             {
                 adj_list[i].push_back(graph[i][j]);
             }
         }
-        
+        */
         each_path.push_back(0);
         dfs(adj_list , 0 , n-1, each_path);
         
