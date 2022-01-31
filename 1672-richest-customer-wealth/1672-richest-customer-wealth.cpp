1e62f19d835f6884bcd02bbc92342f92ab7c445d
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-01-31 09:41:09 +0530

    Time: 12 ms (14.96%), Space: 7.8 MB (86.51%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,20 +3,22 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) 
     {
         int wealth = INT_MIN;
+        
         int row = accounts.size();
+        
         int col = accounts[0].size();
         
-        for(int i=0; i<row; i++)
+        for(int i=0; i<row; i++)  // pointing each row
         {
-            int max_sum=0;
-            for(int j=0; j<col; j++)
+            int richest_customer_wealth = 0;
+            
+            for(int j=0; j<col; j++)  // for each row --> add all column value
             {
-                max_sum +=accounts[i][j];
+                richest_customer_wealth +=accounts[i][j];
             }
-            wealth = max(wealth , max_sum);
+            
+            wealth = max(wealth , richest_customer_wealth);  // compare for maximum wealth
         }
-        
-        
         return wealth;
     }
 };
