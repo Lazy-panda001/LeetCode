8539824427802ea9b8c0e166b3090b850fec032e
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-04 20:11:56 +0530

    Time: 128 ms (68.09%), Space: 83.9 MB (25.19%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) 
     {
-        unordered_map<int,int>ump;
-        int max_length = 0;
         int curr_sum = 0;
-        
+        int max_length = 0;
+        unordered_map<int,int> ump;
         for(int i=0; i<nums.size(); i++)
         {
-            if(nums[i] == 0)
-            {
-                curr_sum += (-1);
-            }
-            else
-            {
-                curr_sum += 1;
-            }
+            curr_sum += nums[i] == 0 ? -1 : 1;
             
-            //
             if(curr_sum == 0)
             {
-                max_length = max(max_length , i+1);
+                max_length = max(max_length,i+1);
             }
-            else if(ump.find(curr_sum) != ump.end())
+            
+            if(ump.find(curr_sum) != ump.end())
             {
                 max_length = max(max_length , i - ump[curr_sum]);
             }
@@ -31,6 +23,7 @@ public:
                 ump[curr_sum] = i;
             }
         }
+        
         return max_length;
         
     }
