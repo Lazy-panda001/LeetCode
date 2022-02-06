740efead1c8ae7c9c1f54b2078d0c889b8be49b5
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-06 11:46:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (25.00%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     long long smallestNumber(long long num) 
     {
         if(num == 0)
@@ -11,24 +12,9 @@ public:
         
         if(num < 0) // means negative value
         {
-            num = num * (-1); // making as positive
-            
-            while(num > 0)
-            {
-                digits.push_back(num % 10);
-                num = num / 10;
-            }
-            
-            sort(digits.begin(), digits.end(), greater<int>());
-            
-            long long number = 0;
-            
-            for(int i=0; i<digits.size(); i++)
-            {
-                number = number*10 + digits[i];
-            }
-            
-            return number*(-1);
+            string str = to_string(num);
+            sort(str.begin()+1 , str.end(), greater<int>());
+            return stoll(str);
             
         }
         else
@@ -39,7 +25,7 @@ public:
                 num = num / 10;
             }
             
-            sort(digits.begin(), digits.end());
+            sort(digits.begin(), digits.end());  // sort in increasing order
             
             long long zeros = 0;
             
@@ -58,7 +44,7 @@ public:
             
             for(int j=i+1; j<digits.size(); j++)
             {
-                number = number*10 + digits[j];
+                number = number*10 + digits[j];  // now all non-zeros will be added
             }
             
             return number;
