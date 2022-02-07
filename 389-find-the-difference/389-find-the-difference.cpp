15609e8fea9ffbbf6831d3b8d8c6610c72ee0231
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-07 08:58:46 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        string str = s+t;
+        string str = s+t;  // make a single string
+        
+        // now in str mostly all character repeat twice except one character and that it the answer
         cout<<str<<endl;
         
         char single = 0;
         
-       for(auto ch : str)
+       for(auto ch : str)   // now simple traverse and find XOR (for finding single character)
         {
             single = single ^ ch;
         }
