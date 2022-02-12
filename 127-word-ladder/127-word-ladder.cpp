3b4a21d6a8adfb90f9c36dd87b6850c04b84939a
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-12 21:05:54 +0530

    Time: 113 ms (78.44%), Space: 13.8 MB (85.14%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,47 +1,51 @@
-class Solution 
-{
+class Solution {
 public:
     int ladderLength(string Begin, string End, vector<string>& wordList) 
     {
         unordered_set<string> dict(wordList.begin(), wordList.end());
-        if(dict.find(End)==dict.end())
-        {
+        
+        if(dict.find(End) == dict.end()) // means end string is not present
             return 0;
-        }
-        queue<string>qu;
+        
+        queue<string> qu;
         qu.push(Begin);
         int steps=1;
-        while(!qu.empty())
+        
+       while(!qu.empty())
         {
-            int size=qu.size();
-         
-            for(int i=0; i<size; i++)
-            {
-                string str=qu.front();
-                qu.pop();
-                
-                if(str==End)
+           
+           int size = qu.size();
+           
+           for(int i=0; i<size; i++)
+           {
+               string str = qu.front(); qu.pop();
+               
+                if(str == End)
                 {
-                    return steps;
+                    return steps;  // breaking point
                 }
-                for(int i=0;i<str.size();i++)
-                {
-                    for(char ch ='a'; ch <='z';ch++)
-                    {
-                        char c=str[i];
-                        str[i]=ch;
-                        if(dict.find(str)!=dict.end())
-                        {
-                            dict.erase(str);
-                            qu.push(str);
-                        }
-                        str[i]=c;
-                    }
-                }
-              
-            }
+               
+               for(int i=0; i<str.size(); i++)
+               {
+                   for(char ch = 'a' ; ch<='z'; ch++)
+                   {
+                       char c = str[i]; // 0 , 1 , 2
+                       str[i] = ch; // modify
+                       
+                       if(dict.find(str) != dict.end())  // present in dictionary
+                       {
+                           dict.erase(str);
+                           qu.push(str);
+                       }
+                       
+                       // again got bach to previous string
+                       str[i] = c;
+                   }
+               }
+           }
             steps++;
         }
         return 0;
+        
     }
 };
