c62d3d1adc313d4d382b6e13f137c3bdc913ecad
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-15 21:04:00 +0530

    Time: 297 ms (70.06%), Space: 100.8 MB (81.31%) - LeetHub

---
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long minimumRemoval(vector<int>& beans) 
+    {
+        
+        long long n = beans.size();
+        
+        sort(beans.begin(), beans.end());
+        
+        long long total_sum = 0;
+        
+        for(auto it : beans) total_sum +=it;
+        
+        long long curr_sum = 1e18;
+        
+        for(long long i=0; i<n; i++)
+        {
+            long long curr = total_sum - (n-i)*(long long)beans[i] * 1LL;
+            curr_sum = min(curr_sum , curr);
+        }
+        
+        return curr_sum;
+    }
+};
