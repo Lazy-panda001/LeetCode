aa8ad6941112f1056cf8429eb636e2237fc7348c
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-21 09:05:30 +0530

    Time: 33 ms (33.29%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,20 +2,41 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) 
     {
-        int n = nums.size();
-        unordered_map<int,int> ump;
-        for(int i=0; i<n;i++)
+        int value = nums[0]; // initially let this is the " possible majority element
+        int count=1;
+        
+        for(int i=1; i<nums.size(); i++)
         {
-            ump[nums[i]]++;
+            if(value == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--; // not match
+            }
+            
+            if(count == 0)  // if count becomes zero 
+            {
+                value = nums[i]; // new "possible  majority element"
+                count++;
+            }
         }
         
+        int count_majority = 0;  // finally it will count majority element 
         
-        for(auto it:ump)
+        for(auto it : nums)
         {
-            if(it.second > ceil(n/2))
+            if(value == it)
             {
-                return it.first;
+                count_majority++;
             }
+        }
+        
+        //check majority condition according to question
+        if(count_majority > ceil(nums.size() / 2))
+        {
+            return value;
         }
         return -1;
         
