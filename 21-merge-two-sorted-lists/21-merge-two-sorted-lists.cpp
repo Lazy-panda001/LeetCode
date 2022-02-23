5d5bfa0ae1eb7291c2854c1909ebdb576135751d
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-23 18:12:45 +0530

    Time: 10 ms (57.09%), Space: 14.8 MB (45.32%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -26,34 +26,17 @@ public:
             return head1;
         }
         
-        ListNode* dummy = new ListNode(-1);
-        ListNode* prev = dummy;
-        
-        while(head1 and head2)
+        if(head1->val < head2->val)
         {
-            if(head1->val < head2->val)
-            {
-                prev->next = head1;
-                head1 = head1->next;
-                prev = prev->next;
-            }
-            else
-            {
-                prev->next = head2;
-                head2 = head2->next;
-                prev = prev->next;
-            }
+            head1->next = mergeTwoLists(head1->next , head2);
+            return head1;
+        }
+        else
+        {
+            head2->next = mergeTwoLists(head1 , head2->next);
+            return head2;
         }
         
-        if(head1)
-        {
-            prev->next = head1;
-        }
-        else if(head2)   
-        {
-            prev->next = head2;
-        }
-        
-        return dummy->next;
+       
     }
 };
