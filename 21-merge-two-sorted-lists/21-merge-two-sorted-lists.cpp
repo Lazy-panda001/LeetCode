4e543fb7648217b5e5a5e8bfc54a9d559c970666
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-23 18:08:03 +0530

    Time: 16 ms (24.83%), Space: 14.9 MB (45.32%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -12,53 +12,48 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) 
     {
-         // code here
-    if(head1 == nullptr and head2 == nullptr)
-    {
-        return nullptr;
-    }
-    
-    if(head1 == nullptr)
-    {
-        return head2;
-    }
-    if(head2 == nullptr){
-        return head1;
-    }
-    
-    ListNode* dummy = new ListNode(-1);
-    ListNode* prev = dummy;
-    
-    ListNode* temp1 = head1;
-    ListNode* temp2 = head2;
-    
-    while(temp1 and temp2)
-    {
-        if(temp1->val < temp2->val)
+        if(head1 == nullptr and head2==nullptr)
         {
-            prev->next =temp1;
-            temp1 = temp1->next;
-            prev = prev->next;
+            return nullptr;
         }
-        else
-        {
-            prev->next =temp2;
-            temp2 = temp2->next;
-            prev = prev->next;
-        }
-    }
-    
-    // if any one becomes null
-    if(temp1 != nullptr)
-    {
-        prev->next = temp1;
-    }
-    else
-    {
-        prev->next = temp2;
-    }
-    
-    return dummy->next;
         
+        if(head1 == nullptr)
+        {
+            return head2;
+        }
+        if(head2 == nullptr)
+        {
+            return head1;
+        }
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* prev = dummy;
+        
+        while(head1 and head2)
+        {
+            if(head1->val < head2->val)
+            {
+                prev->next = head1;
+                head1 = head1->next;
+                prev = prev->next;
+            }
+            else
+            {
+                prev->next = head2;
+                head2 = head2->next;
+                prev = prev->next;
+            }
+        }
+        
+        if(head1)
+        {
+            prev->next = head1;
+        }
+        else   
+        {
+            prev->next = head2;
+        }
+        
+        return dummy->next;
     }
 };
