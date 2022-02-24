77b40798700019236307ee331c920d8019c74c81
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-02-24 09:36:36 +0530

    Time: 152 ms (43.98%), Space: 48.5 MB (32.24%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,79 +10,85 @@
  */
 class Solution {
 public:
-  ListNode* middle(ListNode* head)  // find mid 
- {
-    if(head == nullptr || head->next == nullptr)
+    ListNode* middle(ListNode* head)
     {
-        return head;
-    }
-    ListNode* slow = head;
-    ListNode* fast = head;
-    
-    while(fast->next and fast->next->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    return slow;
- }
- 
- ListNode* mergeList(ListNode* l1 , ListNode* l2) // merge code
- {if(l1 == nullptr and l2 == nullptr) 
-      {
-        return nullptr;
-      }
-  
-      if(l1 == nullptr) return l2;
-      if(l2 == nullptr) return l1;
-    
-      
-      ListNode* dummy = new ListNode(-1);
-      ListNode* prev = dummy;
-      
-      ListNode* temp1 = l1;
-      ListNode* temp2 = l2;
-      
-      while(temp1 != nullptr and temp2 != nullptr)
-      {
-        if(temp1->val < temp2->val)
+        if(head == nullptr or head->next == nullptr)
         {
-          prev->next = temp1;
-          temp1 = temp1->next;
+            return head;
         }
-        else
-        {
-          prev->next = temp2;
-          temp2 = temp2->next;
-        }
-       prev = prev->next;   
-      }
-      
-      if(temp1 != nullptr)
-      {
-        prev->next = temp1;
-      }
-      else
-        prev->next = temp2;
-     // prev->next = temp1 ? temp1 : temp2;
-      
-    return dummy->next;
- }
-    ListNode* sortList(ListNode* head)  // merge sort
-    {
-      if(head == nullptr or head->next == nullptr)
-      {
-        return head;
-      }
-      
-      ListNode* mid = middle(head);
-      ListNode* nhead = mid->next;
-      mid->next = nullptr;
-      
-      ListNode* l1 = sortList(head);
-      ListNode* l2 = sortList(nhead);
-      
-      return mergeList(l1,l2);
         
+        ListNode* slow = head, *fast = head;
+        
+        while(fast->next and fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+    
+    ListNode* merge(ListNode* head1 , ListNode* head2)
+    {
+        if(head1 == nullptr and head2 == nullptr)
+        {
+            return nullptr;
+        }
+        if(head1 == nullptr)
+        {
+            return head2;
+        }
+        if(head2 == nullptr)
+        {
+            return head1;
+        }
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* prev = dummy;
+        
+        ListNode* temp1 = head1;
+        ListNode* temp2 = head2;
+        while(temp1 and temp2)
+        {
+            if(temp1->val < temp2->val)
+            {
+                prev->next = temp1;
+                temp1 = temp1->next;
+            }
+            else
+            {
+                prev->next = temp2;
+                temp2 = temp2->next;
+            }
+            prev = prev->next;
+        }
+        
+        if(temp1 != nullptr)
+        {
+            prev->next = temp1;
+        }
+        if(temp2 != nullptr)
+        {
+            prev->next = temp2;
+        }
+        
+        return dummy->next;
+    }
+    
+    ListNode* sortList(ListNode* head) 
+    {
+        if(head == nullptr or head->next == nullptr)
+        {
+            return head;
+        }
+        
+        ListNode* mid = middle(head);
+        ListNode* nhead = mid->next;
+        mid->next = nullptr;
+        
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(nhead);
+        
+       return merge(l1,l2);
     }
 };
