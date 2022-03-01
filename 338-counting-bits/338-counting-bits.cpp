17bbbaf6fda3c4428f509da5ebca63aab9e79830
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-01 11:14:58 +0530

    Time: 3 ms (95.58%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-  int count_Set(int n)
-  {
-    int count=0;
-    while(n)
+    vector<int> countBits(int n) 
     {
-      count++;
-      n = n & (n-1);
-    }  
-    return count;
-    
-  }
-  
-    vector<int> countBits(int num) 
-    {
-      vector<int> res;
-      for(int i=0; i<=num; i++)
-      {
-        int count = count_Set(i);
-        res.push_back(count);
-      }
-      return res;
+        vector<int> res(n+1);
+        
+        res[0] = 0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            int sum=0;
+            int num = i;
+            
+            while(num !=0)
+            {
+                sum += num % 2;
+                num = num /2;
+            }
+            res[i] = sum;
+        }
+        return res;
         
     }
 };
