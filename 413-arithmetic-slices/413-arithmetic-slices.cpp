e30bfe3c37319912bc529a0ddd4d469b4b704203
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-03 08:23:34 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (88.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,23 +3,34 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) 
     {
         int n = nums.size();
-        if(n < 3)
+        
+        if(n<3)
         {
             return 0;
         }
         
-        int total_subarray = 0;
-        
-        vector<int> dp(n,0);
+        int total_subarray=0;
+        int prev_diff = nums[1] - nums[0], curr_diff=0;
+        int total_count=0;
         
         for(int i=2; i<n; i++)
         {
-            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2])
+            curr_diff = nums[i] - nums[i-1];
+            if(curr_diff == prev_diff)
             {
-                dp[i] = dp[i-1] + 1;
-                total_subarray +=dp[i];
+                ++total_count;
             }
+            
+            else
+            {
+                prev_diff = curr_diff;
+                total_count=0;
+            }
+            
+            total_subarray += total_count;
+            
         }
+        
         
         return total_subarray;
     }
