27381bc9b1ddce002a5b3a420a528317c0edc657
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-21 10:45:21 +0530

    Time: 10 ms (7.34%), Space: 9.1 MB (30.15%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
-      int i = m-1 ; // pointing to last element in nums1
-      int j = n-1; // pointing to last element in nums2;
-      int k = m + n -1;  // pointing to stored position in nums1
-      
-      while(i >=0 and j >=0)
-      {
-        if(nums1[i] < nums2[j])
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        
+        while(i>=0 and j>=0)
         {
-          nums1[k] = nums2[j];
-          k--;
-          j--;
+            if(nums1[i] < nums2[j])
+            {
+                nums1[k--] = nums2[j--];
+            }
+            else
+            {
+                nums1[k--] = nums1[i--];
+            }
         }
-        else
+        
+        while(i>=0)
         {
-          nums1[k] = nums1[i];
-          i--;
-          k--;
+            nums1[k--] = nums1[i--];
         }
-      }
-      // if j brcomes 0
-      while(i>=0)
-      {
-        nums1[k--] = nums1[i--];
-      }
-      
-      // if i becomes 0
-      while(j>=0)
-      {
-        nums1[k--] = nums2[j--];
-      }
-      
-      return;
+        while(j>=0)
+        {
+            nums1[k--] = nums2[j--];
+        }
+        return;
+        
     }
 };
