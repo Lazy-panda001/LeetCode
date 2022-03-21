086c031131bb41823aa3fc6ab9d60ca6b5d48d51
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-21 11:09:36 +0530

    Time: 9 ms (60.82%), Space: 6.9 MB (56.60%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) 
+    {
+        string str ="";
+        int count=0;
+        while(str.size() < b.size())
+        {
+            str +=a;
+            count++;
+        }
+        
+        if(str.find(b) != string::npos) // same size wala issue
+        {
+            return count;
+        }
+        
+        str +=a;
+        count++;
+        
+        if(str.find(b) != string :: npos)
+        {
+            return count;
+        }
+        return -1;
+        
+    }
+};
