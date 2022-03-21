8b5f63637bf968b24f5d87557c84d61e8fe933e9
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-21 10:36:00 +0530

    Time: 26 ms (48.80%), Space: 10.7 MB (55.84%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-      unordered_map<int,int> ump;
-      vector<int> res;
-      for(int i=0; i<nums.size(); i++)
-      {
-        if(ump.find(target - nums[i]) != ump.end())
+        unordered_map<int,int> ump;
+        vector<int> res;
+        
+        for(int i=0; i<nums.size(); i++)
         {
-            res.push_back(ump[target - nums[i]]);
-            res.push_back(i);
-            break;
+            if(ump.find(target - nums[i]) != ump.end())
+            {
+                res.push_back(ump[target-nums[i]]);
+                res.push_back(i); 
+                break;
+            }
+            else
+            {
+                ump[nums[i]] =i; // key --> array element, value--> array element ka corresponding index
+            }
         }
-        else
-        {
-          ump[nums[i]] = i;
-        }
-      }
-      return res;  
+        
+        return res;
+        
     }
 };
