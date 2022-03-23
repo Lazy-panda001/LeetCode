47d3d8d731c329070464df06298d910cda0151a7
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-23 16:42:52 +0530

    Time: 340 ms (6.63%), Space: 96.2 MB (99.00%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& nums) 
     {
-        if(is_sorted(nums.begin(), nums.end(), greater<int>()) || is_sorted(nums.begin(), nums.end(), less<int>()))
-        {
-            return true;
-        }
-        else
-            return false;
+        // 1. make 2 value increase and decrease as 0;
+        // count increase and decrease , if any of the this o=is equal to nums.size() -1 means true else false
         
+        int increase = 0;
+        int decrease = 0;
+        
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i] <= nums[i+1])
+                increase++;
+            if(nums[i] >= nums[i+1])
+                decrease++;
+        }
+        
+        if(increase+1 == nums.size() or decrease+1 == nums.size())
+            return true;
+        return false;
     }
 };
