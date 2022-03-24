a76a509559bf0b1a7832ed2e42f5dbe8b5abdd44
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-03-24 10:34:48 +0530

    Added solution - LeetHub

---
+++ Is Sudoku Valid - GFG/is-sudoku-valid.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int isValid(vector<vector<int>> board)
+    {
+        // code here
+         unordered_set<string> st;
+        
+        int r = board.size();
+        int c = board[0].size();
+        
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                if(board[i][j] != 0)
+                {
+                    int num = board[i][j];
+                    
+                    string row_cel =  "row"+ to_string(i)+ to_string(num);
+                    string col_cel =  "col"+ to_string(j)+ to_string(num);
+                    string box_cel = "box" + to_string(i/3*3+j/3) + to_string(num);
+                    
+                    if(st.find(row_cel) != st.end() || st.find(col_cel) != st.end() || st.find(box_cel) != st.end())
+                    {
+                        return false;
+                    }
+                    st.insert(row_cel);
+                    st.insert(col_cel);
+                    st.insert(box_cel);
+                }
+                    
+            }
+        }
+        
+        return true;   
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<vector<int>> mat(9, vector<int>(9, 0));
+        for(int i = 0;i < 81;i++)
+            cin>>mat[i/9][i%9];
+        
+        Solution ob;
+        cout<<ob.isValid(mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
