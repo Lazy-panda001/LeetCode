f8cec87416462108d8726c7c25588db90e0c8c50
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-06-11 11:15:23 +0530

    Time: 284 ms (57.25%), Space: 98.6 MB (71.00%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) 
+    {
+        int total_sum = 0;
+        for(auto val : nums)
+        {
+            total_sum +=val;
+        }
+        
+        int target_sum = total_sum - x;
+        int curr_sum = 0;
+        
+        int start_idx = 0;
+        
+        int min_opr = 0;
+        bool flag = false;
+        
+        // Sliding Window Technique
+        for(int end_idx = 0; end_idx < nums.size(); end_idx++)
+        {
+            curr_sum +=nums[end_idx]; // step 1  --> formal operation
+             
+            while(start_idx <= end_idx and curr_sum > target_sum)  // step 2 --> negative chance
+            {
+                curr_sum -= nums[start_idx];
+                start_idx++;
+            }
+            
+            if(curr_sum == target_sum)
+            {
+                flag = true;
+                min_opr = max(min_opr , end_idx - start_idx + 1);
+            }
+        }
+        
+        if(flag == false)
+            return -1;
+        return nums.size() - min_opr;
+        
+    }
+};
