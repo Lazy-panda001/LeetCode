a6eb747f9837b003bbc679700115db280cbd4fa6
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-06-28 09:02:27 +0530

    Time: 58 ms (86.76%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minDeletions(string s) 
     {
+        /*
         unordered_map<char,int> char_counts;
         
         for(auto ch : s)
@@ -31,5 +32,34 @@ public:
         }
         
         return ans;
+        */
+        
+        // total - keep = not keep
+        
+        vector<int>freq(26,0);
+        for(int i=0; i<s.size(); i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        
+        sort(freq.begin(), freq.end(), greater<int>());
+        
+        int prev = INT_MAX;
+        int keep=0;
+        
+        for(int i=0; i<26; i++)
+        {
+            int f = freq[i];
+            
+            if(f == 0 or prev == 0)
+            {
+                break;
+            }
+            
+            prev = min(f, prev-1);
+            
+            keep +=prev;
+        }
+        return s.size() - keep;
     }
 };
