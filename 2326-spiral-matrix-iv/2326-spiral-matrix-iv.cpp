bd40a485b43e0d69cb23bfae66fee3761e5da0f5
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-07-03 12:56:36 +0530

    Time: 441 ms (33.33%), Space: 127.9 MB (66.67%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) 
+    {
+        vector<vector<int>>mat(m,vector<int>(n,-1));
+        
+        int minr = 0;
+        int maxr = m-1;
+        int minc = 0;
+        int maxc = n-1;
+        
+        ListNode* temp = head;
+        
+        int cnt = 0;
+        int tot = m*n;
+        
+        while(cnt < tot)
+        {
+            // top filling
+            for(int j=minc, i=minr; j<=maxc and temp != nullptr and cnt < tot; j++)
+            {
+                mat[i][j] = temp->val;
+                temp = temp->next;
+                cnt++;
+            }
+            
+             if(temp == nullptr or cnt == tot)
+            {
+                break;
+            }
+            
+            minr++;
+            
+           
+            // right filling
+            for(int i= minr, j=maxc; i<=maxr and temp!= nullptr and cnt<tot; i++)
+            {
+                mat[i][j] = temp->val;
+                temp = temp->next;
+                cnt++;
+            }
+            
+            if(temp == nullptr or cnt == tot)
+            {
+                break;
+            }
+            maxc--;
+           
+            
+            // bottom filling
+            for(int j=maxc, i=maxr; j>=minc and temp != nullptr and cnt < tot ; j--)
+            {
+                mat[i][j] = temp->val;
+                temp = temp->next;
+                cnt++;
+            }
+           
+             if(temp == nullptr or cnt == tot)
+            {
+                break;
+            }
+            maxr--;
+            
+            // left filling
+            for(int i=maxr, j = minc; i>=minr and temp != nullptr and cnt < tot ; i--)  
+            {
+                 mat[i][j] = temp->val;
+                temp = temp->next;
+                cnt++;
+            }
+            
+            minc++;
+        }
+        
+        return mat;
+        
+    }
+};
