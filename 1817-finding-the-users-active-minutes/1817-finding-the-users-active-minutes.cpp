770bfe530a0e46e110400020bcff62e0b797b74e
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-07-08 18:28:19 +0530

    Time: 567 ms (16.98%), Space: 106.6 MB (13.21%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
@@ -4,7 +4,7 @@ public:
     {
         unordered_map<int, unordered_set<int>> ump;
         
-        vector<int> res(k+1,0);
+        vector<int> res(k,0);
         
         for(auto it : logs)
         {
@@ -13,16 +13,18 @@ public:
         
         for(auto it : ump)
         {
-            int s = it.second.size();
+            int s = it.second.size()-1;
             res[s]++;
             s = 0;
         }
         
+        return res;
+        /*
         vector<int> ans;
         for(int i=1; i<=k; i++)
         {
             ans.push_back(res[i]);
         }
-        return ans;
+        return ans*/
     }
 };
