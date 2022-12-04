a0abcf790bc3caa3c09a4277e0d3e087a05d0d89
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2022-12-04 20:03:43 +0530

    Time: 6 ms (50.00%), Space: 6.8 MB (60.00%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) 
+    {
+        int n = s.size();
+        
+        if(s[0] != s[n-1])
+        {
+            return false;
+        }
+        
+        int i=0;
+        
+        while(i < n)
+        {
+            if(s[i] == ' ' && s[i-1] != s[i+1])
+            {
+                return false;
+            }
+            else
+            {
+                // do nothing
+            }
+        i++;
+        }
+        
+        return true;
+       
+    }
+};
