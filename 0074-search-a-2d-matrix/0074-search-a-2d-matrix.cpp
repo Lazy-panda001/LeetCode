0945b902b5988d414220a2c57d6197fe821e3c28
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2023-08-07 11:36:26 +0530

    Time: 6 ms (33.67%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,24 +6,42 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         
-        int low = 0;
-        int high = col - 1;
         
-        while(low < row and high >= 0 )
-        {
-            if(matrix[low][high] > target)
-            {
-                high--;
-            }
-            else if(matrix[low][high] < target)
-            {
-                low++;
-            }
-            else
-            {
+// ***************        TC -- > O(n2)
+//         int low = 0;
+//         int high = col - 1;
+        
+//         while(low < row and high >= 0 )
+//         {
+//             if(matrix[low][high] > target)
+//             {
+//                 high--;
+//             }
+//             else if(matrix[low][high] < target)
+//             {
+//                 low++;
+//             }
+//             else
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+        int start = 0;
+        int end = row*col - 1;
+        
+        while(start <=end) {
+            int mid = start + (end - start) / 2;
+            
+            if(matrix[mid/col][mid%col] > target) {
+                end = mid-1;
+            } else if(matrix[mid/col][mid%col] < target) {
+                start = mid + 1;
+            } else
                 return true;
-            }
         }
         return false;
+        
     }
 };
