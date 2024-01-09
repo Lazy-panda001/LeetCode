609ad57d10d357645add70191d0c05b047d0f6b2
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2024-01-09 10:39:16 +0530

    Time: 7 ms (20.29%), Space: 13.3 MB (30.37%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div872-leaf-similar-trees/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div872-leaf-similar-trees.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void func(TreeNode* root, vector<int>&res) {
+        if(root == nullptr) {
+            return;
+        }
+        
+        // leaf condition
+        if(root->left == nullptr and root->right == nullptr) {
+            res.push_back(root->val);
+        }
+        
+        func(root->left, res);
+        func(root->right, res);
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        if(root1 == nullptr and root2 == nullptr) {
+            return true;
+        }
+        
+        if(root1 == nullptr) {
+            return false;
+        }
+        
+        if(root2 == nullptr) {
+            return false;
+        }
+        
+        vector<int> tree1;
+        vector<int> tree2;
+        
+        func(root1,tree1);
+        func(root2,tree2);
+        
+        if(tree1.size() != tree2.size())
+        {
+            return false;
+        }
+        
+        for(int i=0; i<tree1.size(); i++)
+        {
+            if(tree1[i] != tree2[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
